7fc5fa374078d6f2b122e16e60d0ecf5c1d5fab7
Gregory Mason <chevron36@gmail.com>
2021-07-25 15:15:37 -0500

    Create basic GUI, remove original loading code

--- ElectricEstimate.js
+++ ElectricEstimate.js
@@ -11,49 +11,11 @@
 
 (function() {
     'use strict';
-    return;
-    var loader=document.getElementById("page_loader");
-    var navigateDate = function(day) {
-        //simulate clicks through UI. Slower but doesn't DOS website (or myself)
-        // url: "Usages.aspx/LoadUsage", is the ajax call
-        if (loader.style.display === "none"){
-            if (day){
-                if (!day.previousElementSibling) {
-                    day=day.parentElement.previousElementSibling.lastElementChild;
-                }
-                else
-                {
-                    day=day.previousElementSibling;
-                }
-            }
-            else
-            {
-                var calendar = document.querySelector(".ui-datepicker-calendar");
-                var newestDay = calendar.firstChild.nextElementSibling.lastElementChild.lastElementChild;
-                //performance improvement: skip inacctive days
-                day = newestDay;
-            }
-            day.click();
-        }
-
-        setTimeout(function() {
-            if (window.kill) { return;}
-            if (window.pause) { debugger;}
-            navigateDate(day);
-        },1000); // while loading wait
-    };
-    var navToHourly = function() {
-        if (loader.style.display !== "none"){
-            setTimeout(navToHourly,1000); // while loading wait
-        }
-        //billing rate is at specific times, need to select right rate;
-        var hourly = document.querySelector("[globalize=\"ML_Usage_Btn_Hourly\"");
-        hourly.click(); // need to wait for next part of page to load
-        setTimeout(navigateDate, 1000);
-    }
-
-    setTimeout(function() {
-        navToHourly();
-     },1000);
+    var GUI=document.createElement("div");
+	GUI.style.position="fixed";
+	GUI.style.zIndex="999";
+	div.style.width="200px";
+	div.style.height="100px";
+	document.body.insertBefore(GUI,document.body.children[0]);
     // Your code here...
 })();
