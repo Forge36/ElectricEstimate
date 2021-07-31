d7a0ee02c9ac0d6fb08caf183cf5d5f6dabe7edb
Gregory Mason <chevron36@gmail.com>
2021-07-31 15:56:05 -0500

    Interactive GUI, meterNumber can be looked up
    GUI - Closes #3
    	Remove fixed position GUI add into existing left toolbar
    	Add load older+newer buttons. Closes #5 and Closes #4
    	Add oldest/newest entry field
    peak start/end are constants move outside of cost types
    fix off-by one with new data requests. timeZone added to DateTimeFormat ref dateFormatter
    fix off-by one with days of data to load.
    functions for isPeak hour calculation, getSeasonRate information

--- ElectricEstimate.js
+++ ElectricEstimate.js
@@ -12,12 +12,8 @@
 (function() {
     'use strict';
     var GUI=document.createElement("div");
-	GUI.style.position="fixed";
-	GUI.style.zIndex="999";
-	GUI.style.width="300px";
-	GUI.style.height="100px";
-	GUI.style.marginTop="600px";
-	document.body.insertBefore(GUI,document.body.children[0]);
+    var leftNam = document.querySelector(".energy_mid_box .nav_left");
+	leftNam.appendChild(GUI);
 
     var meterNumber;
 	var dateData = {};
@@ -25,18 +21,29 @@
     if (localStorage.dateData){
         dateData = JSON.parse(localStorage.dateData); // retrieve saved values to avoid excessive requests
 
-        let dateIndex = Object.getOwnPropertyNames(dateData); // +3 Update date request to have oldest data
-        let oldest=dateIndex[dateIndex.length -1]; // assumes descending sort
-        date = new Date(oldest); // when cache data exist set load start to today
-		// TODO: support fetching data newer than first run
+        let dateList = Object.getOwnPropertyNames(dateData); // +3 Update date request to have oldest data
+        var oldestDate = new Date(dateList[0]);
+        var newestDate = new Date(dateList[0]);
+        for (var dateIndex =1;dateIndex<dateList.length;dateIndex++){
+            let tmp = new Date(dateList[dateIndex]);
+            if (oldestDate > tmp){
+            	oldestDate=tmp;
+            }
+            if (newestDate < tmp){
+                newestDate = tmp;
+            }
+        }
     }
-
-    var days = -1; // lets not spam requests until this is done
+    else{
+        oldestDate = new Date();
+        newestDate = new Date();
+    }
 
     let dateFormatter = new Intl.DateTimeFormat("en" , {
         year: "2-digit",
         month: "2-digit",
         day: "2-digit",
+        timeZone:"UTC",
     });
 
     // DemandValue: 2.87
@@ -61,90 +68,135 @@
         }
     }
 
+    var peakHourStart = "07:00";
+    var peakHourEnd = "20:00";
     var costTypes= [{
         Start: "05/16",
         End: "09/15",
-        PeakStart: "07:00",
         Standard: 0.11685,
         PeakRate: 0.16359,
-        PeakEnd: "20:00",
         OffPeak: 0.05843,
     },
 	{
 		Start: "09/16",
 		End: "05/15",
-		PeakStart: "07:00",
 		Standard: 0.09969,
 		PeakRate: 0.13957,
-		PeakEnd: "20:00",
 		OffPeak: 0.04985,
 	}
-   ]
+   ];
+    var isPeak = function(usage){
+        var date = new Date(usage.UsageDate);
+        if (date.getDay() === 0 || date.getDay() === 6)
+        {
+            //Off Peak M-F S&S are off-peak
+            return false;
+        }
+        // [0] needs to go not handling season change
+        else if (usage.Hourly.localeCompare(peakHourStart) <1 )
+        {
+            //Hourly is "previous hours" usage, "PeakStart" applies 7 to 8, an hourly of 7 is off peak
+            return false;
+        }
+        else if (usage.Hourly.localeCompare(peakHourEnd) === 1 )
+        {
+            //Hourly of 8 is on-peak and 9 is usage from 8 to 9
+            return false;
+        }
+        else if (false)
+        {
+            //TODO: add holiday exceptions as off=peak;
+            // New Year’s Day, Memorial Day, Independence Day, Labor Day, Thanksgiving Day and Christmas Day
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    var getSeasonRate = function(usageDate){
+        if (usageDate.localeCompare(costTypes[1].End)<1){
+            return costTypes[1]; // January 1st through May 15th (my winter season end)
+        }
+        else if (usageDate.localeCompare(costTypes[0].End)<1){
+            return costTypes[0]; // May 15th Through September 15th (my summer season)
+        }
+        else{
+            return costTypes[1]; // September 16th through EOY
+        }
+    };
     var estimateCost = function(){
         var cost = 0;
         var standardCost = 0;
-        var peak = false;
         for (var usageIndex=0;usageIndex<usageByHour.length;usageIndex++){
             var usage = usageByHour[usageIndex];
-            var date = new Date(usage.UsageDate);
-            var season=-1;
-            if (usage.UsageDate.localeCompare(costTypes[1].End)<1){
-                season=1; // January 1st through May 15th (my winter season end)
-            }
-            else if (usage.UsageDate.localeCompare(costTypes[0].End)<1){
-                season=0; // May 15th Through September 15th (my summer season)
+            var seasonRate = getSeasonRate(usage.UsageDate);
+            standardCost += usage.UsageValue*seasonRate.Standard; // standard rate calculation
+            if (isPeak(usage)){
+                cost += usage.UsageValue*seasonRate.PeakRate;
             }
             else{
-                season=1; // September 16th through EOY
-            }
-            if (date.getDay() === 0 || date.getDay() === 6)
-            {
-                //Off Peak M-F S&S are off-peak
-                peak=false;
-            }
-            // [0] needs to go not handling season change
-            else if (usage.Hourly.localeCompare(costTypes[season].PeakStart) <1 )
-            {
-                //Hourly is "previous hours" usage, "PeakStart" applies 7 to 8, an hourly of 7 is off peak
-                peak=false;
-            }
-            else if (usage.Hourly.localeCompare(costTypes[season].PeakEnd) === 1 )
-            {
-                //Hourly of 8 is on-peak and 9 is usage from 8 to 9
-                peak=false;
-            }
-            else if (false)
-            {
-                //TODO: add holiday exceptions as off=peak;
-                // New Year’s Day, Memorial Day, Independence Day, Labor Day, Thanksgiving Day and Christmas Day
-                peak = false;
-            }
-            else{
-                peak = true;
-            }
-            standardCost += usage.UsageValue*costTypes[season].Standard; // standard rate calculation
-            if (peak){
-                cost += usage.UsageValue*costTypes[season].PeakRate;
-            }
-            else{
-                cost += usage.UsageValue*costTypes[season].OffPeak;
+                cost += usage.UsageValue*seasonRate.OffPeak;
             }
         }
         GUI.innerHTML = "With Off-Peak: " + cost + "</br>Standard Cost: " + standardCost
+
+        var oldestData = document.createElement("input");
+        oldestData.type="date";
+        oldestData.valueAsDate = oldestDate; //oldest data
+		GUI.appendChild(document.createElement("br"));
+		GUI.appendChild(document.createTextNode("Oldest Data: "));
+        GUI.appendChild(oldestData);
+
+        var dateStart = document.createElement("input");
+        dateStart.type="date";
+        dateStart.valueAsDate = newestDate;
+        GUI.appendChild(document.createElement("br"));
+		GUI.appendChild(document.createTextNode("Newest Data: "));
+        GUI.appendChild(dateStart);
+
+        var lookupCount = document.createElement("input");
+        lookupCount.type="number";
+        lookupCount.value =7; // 7 days history by default
+        GUI.appendChild(document.createElement("br"));
+		GUI.appendChild(document.createTextNode("Days to Lookup: "));
+        GUI.appendChild(lookupCount);
+
+        var button = document.createElement("button");
+        button.appendChild(document.createTextNode("Request Older Data"));
+        button.onclick = function() {
+            requestHourlyData(oldestData.valueAsDate,lookupCount.value, -1);
+            return false;
+        };
+        GUI.appendChild(document.createElement("br"));
+        GUI.appendChild(button);
+
+        button = document.createElement("button");
+        button.appendChild(document.createTextNode("Request Newer Data"));
+        button.onclick = function() {
+            requestHourlyData(dateStart.valueAsDate,lookupCount.value, 1);
+            return false;
+        };
+        GUI.appendChild(button);
     }
 
-    var requestHourlyData = function(){
+    var requestHourlyData = function(date, days, direction){
         // should space our requests more
-        date.setDate(date.getDate()-1); // move to previous day
-        if (days<0){
+        if (days<=0){
             localStorage.setItem("dateData",JSON.stringify(dateData)); //save latest data
             flattenResponse();
             estimateCost();
             return; // quit
         }
+        date.setDate(date.getDate() + direction); // move to previous day
+        if (oldestDate > date){
+            oldestDate=date;
+        }
+        if (newestDate < date){
+            newestDate = date;
+        }
         days--;
-        var request =
-            {
+
+        var request = {
                 DateFromDaily: "",
                 DateToDaily: "",
                 MeterNumber: meterNumber,
@@ -157,7 +209,6 @@
                 usageyear: "",
                 weatherOverlay: 0
             }
-
         $.ajax({
             type: "POST",
             url: "Usages.aspx/LoadUsage",
@@ -169,7 +220,7 @@
                 if (usageByHour.length>0 && usageByHour[0].UsageDate){
                     dateData[usageByHour[0].UsageDate]=usageByHour;
                 }
-                setTimeout(requestHourlyData,1000);
+                setTimeout(function() { requestHourlyData(date, days, direction);},100);
             },
             error: function(n) {
                 console.log(n.message);
@@ -177,13 +228,16 @@
             }
         });
     }
-	var ajaxOrig = $.ajax;
+	debugger;
+    var ajaxOrig = $.ajax;
     $.ajax = function() {
 		if (arguments[0] && arguments[0].url === "Usages.aspx/LoadUsage" && !meterNumber){
             // intercept request to load data to cache meterNumber
             var data = JSON.parse(arguments[0].data);
             meterNumber = data.MeterNumber;
-            requestHourlyData();
+
+            // lets not spam requests until this is done
+            requestHourlyData(date,-1);
         }
 		ajaxOrig.apply($,arguments);
 	}
